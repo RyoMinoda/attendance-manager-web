eec613e505f3e1262cd061caa0e3f815ec902745
Ryo Minoda <minoda1019@yahoo.co.jp>
2022-05-15 15:52:37 +0900

    react in typescript preparation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <></>
+    </BrowserRouter>
+  );
+}
+
+export default App;
